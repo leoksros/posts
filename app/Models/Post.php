ea3d836897305229df30286a364462e619315491
leoksros <leonardopmiceli@gmail.com>
2020-11-04 15:53:33 -0300

    fillable attributes added

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+        'image',
+        'category_id',
+        'user_id'
+    ];
 }
