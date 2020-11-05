169cea54525ad96f1b2dd3db81c84e825bde5c39
leoksros <leonardopmiceli@gmail.com>
2020-11-05 01:47:58 -0300

    creation of middleware

---
+++ app/Http/Middleware/PostOwner.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\Post;
+/* use App\Http\Middleware\Auth;*/
+use Illuminate\Support\Facades\Auth;
+
+class PostOwner
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $post = Post::findOrFail(array_values($request->route()->parameters())[0]);
+        
+        
+        if($post->user_id != Auth::user()->id)
+        {
+            return redirect()->route('posts.index');            
+        }
+      
+        return $next($request);
+        
+
+        
+
+    }
+}
