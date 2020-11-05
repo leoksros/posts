1b008aa2d4b9b2c87c59c86d9a9d1d24b72ad2a2
leoksros <leonardopmiceli@gmail.com>
2020-11-05 01:44:12 -0300

    function: relationship with user owner added

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -16,4 +16,14 @@ class Post extends Model
         'category_id',
         'user_id'
     ];
+
+    public function category()
+    {
+        return $this->belongsTo('App\Models\Category');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
 }
