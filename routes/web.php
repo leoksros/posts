3256f3ac2a66f46501257b637f2628b89fb6169a
leoksros <leonardopmiceli@gmail.com>
2020-11-05 01:47:17 -0300

    routes ok

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('posts.index');
+});
+
+Route::get('/home', function() {
+    return redirect()->route('posts.index');
 });
 
 Auth::routes();
 
-/* Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home'); */
-
-
 /* POSTS */
 
-Route::get('/posts/create',[App\Http\Controllers\PostController::class, 'create'])->name('create.post');
-Route::get('/posts',[App\Http\Controllers\PostController::class, 'index'])->name('index.posts');
-Route::post('/posts',[App\Http\Controllers\PostController::class, 'store'])->name('store.post');
-Route::get('/posts/{$id}',[App\Http\Controllers\PostController::class, 'show'])->name('show.post');
-Route::patch('/posts/{$id}',[App\Http\Controllers\PostController::class, 'update'])->name('update.post');
-Route::delete('/posts/{$id}',[App\Http\Controllers\PostController::class, 'destroy'])->name('delete.post');
+Route::resource('posts', PostController::class);
 
 /* CATEGORIES */
 
-Route::get('/categories/create',[App\Http\Controllers\CategoryController::class, 'create'])->name('create.category');
-Route::get('/categories',[App\Http\Controllers\CategoryController::class, 'index'])->name('index.categories');
-Route::post('/categories',[App\Http\Controllers\CategoryController::class, 'store'])->name('store.category');
-Route::get('/categories/{$id}',[App\Http\Controllers\CategoryController::class, 'show'])->name('show.categories');
-Route::patch('/categories/{$id}',[App\Http\Controllers\CategoryController::class, 'update'])->name('update.categories');
-Route::delete('/categories/{$id}',[App\Http\Controllers\CategoryController::class, 'destroy'])->name('destroy.categories');
+Route::resource('categories', CategoryController::class);
+
+
